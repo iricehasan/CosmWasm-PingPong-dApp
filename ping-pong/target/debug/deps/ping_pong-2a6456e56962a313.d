8cef3653503cfd2d40a4ba16ed5cd3ee5dbe7474
iricehasan <hsn.irice@gmail.com>
2023-08-25 15:39:20 +0300

    Init project

---
+++ ping-pong/target/debug/deps/ping_pong-2a6456e56962a313.d
@@ -0,0 +1,5 @@
+/Users/macbookpro/CosmWasm-PingPong-dApp/ping-pong/target/debug/deps/ping_pong-2a6456e56962a313.rmeta: src/lib.rs
+
+/Users/macbookpro/CosmWasm-PingPong-dApp/ping-pong/target/debug/deps/ping_pong-2a6456e56962a313.d: src/lib.rs
+
+src/lib.rs:
---
+++ ping-pong/target/debug/deps/ping_pong-323098b7da5c95b9.d
@@ -0,0 +1,5 @@
+/Users/macbookpro/CosmWasm-PingPong-dApp/ping-pong/target/debug/deps/ping_pong-323098b7da5c95b9.rmeta: src/lib.rs
+
+/Users/macbookpro/CosmWasm-PingPong-dApp/ping-pong/target/debug/deps/ping_pong-323098b7da5c95b9.d: src/lib.rs
+
+src/lib.rs:
